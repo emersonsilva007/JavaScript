f3eb943fbe45613d4db7a6058d89bceecf16ad25
emersonsilva007 <129129191+emersonsilva007@users.noreply.github.com>
2023-10-09 22:27:50 -0300

    0910

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("oi")
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click", dizOi)
+console.log("clique no robô.")
+})
+
+function dizOi(nome) {
+    console.log("Oi" + nome)
+    console.log("Bem-vindo ao Robotron 2000")
+}
+
+dizOi("pedro")
